274259100d04ef4e7bb42a51e2b387de6177eaa6
Rahuletto <rahulmarban@gmail.com>
2024-07-06 19:31:16 +0530

    feat: quiz of the notes

---
+++ app/notes/[id]/quiz/components/QuizCard.tsx
@@ -0,0 +1,102 @@
+import { generateId } from "@/utils/generateId";
+import { Dispatch, RefObject, SetStateAction, useEffect, useRef } from "react";
+
+export default function QuizCard({
+  question,
+  options,
+  answer,
+  num,
+  container,
+  grade,
+  setGrade,
+}: {
+  question: string;
+  grade: (null | "correct" | "wrong")[];
+  options: string[];
+  answer: string;
+  num: number;
+  container: RefObject<HTMLDivElement>;
+  setGrade: Dispatch<SetStateAction<("correct" | "wrong" | null)[]>>;
+}) {
+  const card = useRef<HTMLDivElement>(null);
+
+  const handleIntersection = (entries: IntersectionObserverEntry[]) => {
+    entries.forEach((entry) => {
+      if (!card.current) return;
+      if (entry.isIntersecting && entry.intersectionRatio >= 0.8) {
+        card.current.style.opacity = "1";
+        card.current.style.transform = "scale(1)";
+      } else if(!entry.isIntersecting || entry.intersectionRatio < 0.1) {
+        card.current.style.opacity = "0.3";
+        card.current.style.transform = "scale(0.9)";
+      }
+    });
+  };
+
+  function evaluateAns(e: MouseEvent | any, option: string) {
+    if (generateId(option) === answer) {
+      if (grade[num - 1] === null) grade[num - 1] = "correct";
+      e.target.style.backgroundColor = "var(--green-hue)";
+      e.target.style.color = "var(--green)";
+      e.target.style.borderColor = "var(--green)";
+    } else {
+      if (grade[num - 1] === null) grade[num - 1] = "wrong";
+
+      e.target.style.backgroundColor = "var(--hue)";
+      e.target.style.color = "var(--accent)";
+      e.target.style.borderColor = "var(--accent)";
+    }
+    setGrade([...grade]);
+  }
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(handleIntersection, {
+      root: container.current,
+      rootMargin: "0px",
+      threshold: 0.9,
+    });
+    if (card.current) {
+      observer.observe(card.current);
+    }
+    return () => {
+      if (card.current) {
+        observer.unobserve(card.current);
+      }
+    };
+  }, [card]);
+
+  return (
+    <div
+      ref={card}
+      className={`snap-center border-2 ${
+        grade[num - 1] === "wrong"
+          ? "bg-hue border-accent"
+          : grade[num - 1] === "correct"
+          ? "bg-green-hue border-green"
+          : "bg-category border-bb"
+      } lg:px-12 lg:py-8 h-fit rounded-3xl lg:min-w-[70%] min-w-[96%] px-7 py-5 duration-300 transition-all delay-500`}
+    >
+      <h1 className="text-2xl font-semibold md:block hidden">Question {num}</h1>
+
+      <h2 className="text-lg font-semibold">{question}</h2>
+      <div className="flex flex-col gap-2 md:mt-8 mt-5">
+        {options.map((option, i) => (
+          <div
+            key={i}
+            className="transition-all duration-300 opacity-65 hover:opacity-95 flex gap-3 items-center"
+          >
+            <p className="md:flex hidden text-sm aspect-square rounded-full bg-alt font-semibold items-center justify-center h-6 w-6">
+              {["A", "B", "C", "D"][i]}
+            </p>{" "}
+            <button
+              onClick={(e) => evaluateAns(e, option)}
+              className="text-left flex items-center md:px-6 md:text-lg text-sm px-3 py-2 rounded-xl border-b-8 border-2 border-bb w-full"
+            >
+              {option}
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ app/notes/[id]/quiz/page.tsx
@@ -0,0 +1,129 @@
+"use client";
+import React, { useEffect, useRef, useState } from "react";
+import { getData } from "@/firebase/firestore";
+import { useAuth } from "@/provider/UserProvider";
+import { NoteData } from "@/types/NoteData";
+import { DocumentData } from "firebase/firestore";
+import { useRouter } from "next/navigation";
+import NotFoundError from "@/app/not-found";
+import Loader from "@/components/ui/Loader";
+import { RiLoaderLine } from "react-icons/ri";
+import { QuizData } from "@/types/Quiz";
+import QuizCard from "./components/QuizCard";
+import Back from "@/components/ui/Back";
+
+export default function Quiz({ params }: { params: { id: string } }) {
+  const id = params.id;
+  const user = useAuth();
+  const router = useRouter();
+  const [note, setNote] = useState<DocumentData | null>(null);
+  const [quiz, setQuiz] = useState<QuizData[] | null>(null);
+  const [grade, setGrade] = useState<(null | "correct" | "wrong")[]>([]);
+  const [percent, setPercent] = useState(0);
+
+  const container = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!user) return;
+
+    getData(user.uid, id).then((data) => {
+      if (!data) setNote({ error: true });
+      else setNote(data as NoteData);
+    });
+  }, [user, id]);
+
+  useEffect(() => {
+    if (note && !note.error)
+      fetch("/api/google/quiz", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          prompt: note.transcript,
+        }),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setQuiz(data.result);
+          setGrade(new Array(data.result.length).fill(null));
+        });
+  }, [note]);
+
+  useEffect(() => {
+    setPercent(
+      Math.floor(
+        (grade.filter((g) => g === "correct").length / grade.length) * 100
+      )
+    );
+  }, [grade]);
+
+  if (user == false) router.push("/auth");
+
+  if (!note || !note.title) return <Loader />;
+  if (note.error) return <NotFoundError />;
+  return (
+    <main className="lg:px-8 px-12 lg:py-16 xl:py-24 py-12 max-h-screen flex">
+      <div className="lg:max-w-[85%] xl:max-w-[75%] max-w-[100%] h-fill w-full mx-auto flex flex-col gap-6 transition-all duration-300 animate-fade">
+        <div className="flex items-center gap-4">
+          <Back href={`/notes/${id}`} />
+          <h1 className="lg:text-5xl md:text-4xl text-3xl text-color font-semibold transition-all duration-300">
+            {note.title}
+          </h1>
+        </div>
+        {quiz && quiz.length > 0 ? (
+          <>
+            <div
+              ref={container}
+              id="quizContainer"
+              className="flex scrollbar-thin scrollbar-h-0 flex-row overflow-x-auto snap-x snap-center lg:h-auto h-[69vh] md:h-auto snap-mandatory items-center gap-8 lg:px-4 rounded-3xl"
+            >
+              <div className="md:min-w-[10vw] md:p-10 md:block hidden" />
+              {quiz.map((q, i) => (
+                <QuizCard
+                  setGrade={setGrade}
+                  grade={[...grade]}
+                  num={i + 1}
+                  container={container}
+                  key={i}
+                  question={q.question}
+                  options={q.options}
+                  answer={q.answer}
+                />
+              ))}
+              <div className="md:min-w-[20vw] md:p-10 md:block hidden" />
+            </div>
+            <h2
+              className={`${
+                grade.filter((g) => g === null).length <= 0
+                  ? "text-7xl scale-110"
+                  : "text-5xl scale-100"
+              } mx-auto lg:mt-12 mt-4 ${
+                grade.filter((g) => g === null).length <= 0
+                  ? percent >= 50
+                    ? "text-green"
+                    : "text-accent"
+                  : "text-color"
+              } font-mono font-bold transition-all duration-300`}
+            >
+              {percent}
+              <span className="text-color text-2xl opacity-60 font-sans">
+                %
+              </span>
+            </h2>
+            {grade.filter((g) => g === null).length <= 0 && (
+              <h3 className="text-2xl opacity-80 font-medium text-center w-full transition-all duration-300 animate-fade">
+                Final score
+              </h3>
+            )}
+          </>
+        ) : (
+          <div className="rounded-2xl bg-box border-2 border-bb px-4 py-3 h-72 flex gap-4 items-center justify-center mt-16 w-full mx-auto md:w-[60%]">
+            <RiLoaderLine className="animate-spin duration-1000 transition-all" />
+            <h1 className="text-xl font-semibold">Curating questions..</h1>
+          </div>
+        )}
+      </div>
+    </main>
+  );
+}
